0655c7befaa02802e683968a814fc907333b8414
HP <saiv57177@gmail.com>
2025-07-29 15:41:57 +0530

    Adding one more Employee details

--- src/Object_Oriented_Programmings/Employee.java
+++ src/Object_Oriented_Programmings/Employee.java
@@ -26,8 +26,13 @@ public class Employee
 	{
 		Employee employee = new Employee();
 		
-		employee.setEmployeeData(101, "sai", 3200.0, "kondapur");
+		employee.setEmployeeData(101, "sai", 32000.0, "kondapur");
 		employee.getEmployeeData();
+		
+		System.out.println("================");
+		Employee smith = new Employee();
+		smith.setEmployeeData(222, "Smith", 56000, "Ameerpet");
+		smith.getEmployeeData();
 		
 	}
 
