108d4c6d96ce1e521712439964e162326fa1fe3f
HP <saiv57177@gmail.com>
2025-08-02 21:36:00 +0530

    Instance block will be executed with object creation

---
+++ src/Object_Oriented_Programmings/InstanceBlockDemo01.java
@@ -0,0 +1,16 @@
+package Object_Oriented_Programmings;
+class Sample
+{
+	{
+		System.out.println("Instance block or non-static block");
+	}
+}
+public class InstanceBlockDemo01 
+{
+	public static void main(String[] args) 
+	{
+		new Sample();
+		new Sample();
+		
+	}
+}
