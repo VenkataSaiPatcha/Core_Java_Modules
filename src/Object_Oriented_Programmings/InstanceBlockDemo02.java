b27da04f0dcf3153ce9cf3387d90ec34b6a9b641
HP <saiv57177@gmail.com>
2025-08-02 21:43:38 +0530

    Non-Static-Block will be executed before the constructor body.

---
+++ src/Object_Oriented_Programmings/InstanceBlockDemo02.java
@@ -0,0 +1,20 @@
+package Object_Oriented_Programmings;
+
+class Demo
+{
+	public Demo()
+	{
+		System.out.println("Demo class constructor");
+	}
+	{
+		System.out.println("Instance block or non-static block");
+	}
+}
+public class InstanceBlockDemo02 {
+
+	public static void main(String[] args) 
+	{
+		new Demo();
+	}
+
+}
