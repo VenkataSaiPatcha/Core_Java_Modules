ea6013270998fe9a8c66d3fd5af49a4da961778b
HP <saiv57177@gmail.com>
2025-08-02 22:03:51 +0530

    Non-Static-block is also used to initialize the instance variable.

---
+++ src/Object_Oriented_Programmings/InstanceBlockDemo05.java
@@ -0,0 +1,24 @@
+package Object_Oriented_Programmings;
+
+class Test01
+{
+	int x;
+	public Test01()
+	{
+		x=500;
+		System.out.println("x value is :"+x);
+	}
+	{
+		x=900;
+		System.out.println("X value is :"+x);
+	}
+}
+public class InstanceBlockDemo05 {
+
+	public static void main(String[] args) 
+	{
+		Test01 t1 = new Test01();
+
+	}
+
+}
