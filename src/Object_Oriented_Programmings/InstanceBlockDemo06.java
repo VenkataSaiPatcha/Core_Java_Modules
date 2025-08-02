68e1787695605e359779c02b2a070006ed33c848
HP <saiv57177@gmail.com>
2025-08-02 22:09:24 +0530

    Non static block is executed top to bottom.

---
+++ src/Object_Oriented_Programmings/InstanceBlockDemo06.java
@@ -0,0 +1,40 @@
+package Object_Oriented_Programmings;
+class Seller
+{
+	private double amount;
+	
+	public Seller()
+	{
+		amount=2500.0;
+		System.out.println("Seller selling amount is :"+amount);
+	}
+	{
+		amount=3000.0;
+		System.out.println("Amount :"+amount);
+	}
+	{
+		amount=4000.0;
+		System.out.println("Amount :"+amount);
+	}
+	{
+		amount=5000.0;
+		System.out.println("Amount :"+amount);
+	}
+	{
+		amount=6000.0;
+		System.out.println("Amount :"+amount);
+	}
+	{
+		amount=7000.0;
+		System.out.println("Amount :"+amount);
+	}
+}
+public class InstanceBlockDemo06 
+{
+	public static void main(String[] args) 
+	{
+		new Seller();
+		
+	}
+
+}
