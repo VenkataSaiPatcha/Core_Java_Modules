3194ae52a7ab541d6b4c201a072a1f78473531d7
HP <saiv57177@gmail.com>
2025-08-06 15:37:47 +0530

    we can also initialize the static variable in the static block before declaration and we can acess that variable through class only, otherwise it will throw an error called "illegal forward reference"

---
+++ src/Object_Oriented_Programmings/StaticBlockDemo06.java
@@ -0,0 +1,17 @@
+package Object_Oriented_Programmings;
+class Demo05
+{
+	static
+	{
+		i=100;
+	}
+	static int i;
+}
+public class StaticBlockDemo06
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Demo05.i);
+	}
+
+}
