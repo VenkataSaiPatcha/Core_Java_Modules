0a1adf9efb24bb6bcc781ba68ca87c7b6c6c4771
HP <saiv57177@gmail.com>
2025-08-09 15:39:14 +0530

    We can't call any non static variable in the static block, static method and static nested inner class

---
+++ src/Object_Oriented_Programmings/StaticDemo.java
@@ -0,0 +1,20 @@
+package Object_Oriented_Programmings;
+
+public class StaticDemo 
+{
+	int x;
+	public StaticDemo(int x)
+	{
+		this.x = x;
+	}
+	public static void greet()
+	{
+		System.out.println(x);
+	}
+	public static void main(String[] args) 
+	{
+		StaticDemo s1 = new StaticDemo(10);
+		s1.greet();
+		
+	}
+}
