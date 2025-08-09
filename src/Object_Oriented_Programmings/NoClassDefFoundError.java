6d78a1c7ffbc031ec4724960eb40856deb0c7c3f
HP <saiv57177@gmail.com>
2025-08-09 12:51:18 +0530

    After compilation delete Hello.class file from the current folder and execute the program, we will get java.lang.NoClassDefFoundError.

---
+++ src/Object_Oriented_Programmings/NoClassDefFoundError.java
@@ -0,0 +1,11 @@
+package Object_Oriented_Programmings;
+public class NoClassDefFoundError 
+{
+	public static void main(String[] args) 
+	{
+		Welcome wel = new Welcome();
+		wel.greet();
+		
+	}
+
+}
---
+++ src/Object_Oriented_Programmings/Welcome.java
@@ -0,0 +1,10 @@
+package Object_Oriented_Programmings;
+
+public class Welcome 
+{
+	public void greet()
+	{
+		System.out.println("Welcome to greet method !!");
+	}
+
+}
