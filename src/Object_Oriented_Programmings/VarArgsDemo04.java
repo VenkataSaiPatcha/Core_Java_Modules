70336e54257fdd880f0352be9497a36dcdd2d538
HP <saiv57177@gmail.com>
2025-08-10 21:52:53 +0530

    We can hetrogeneous types of data

---
+++ src/Object_Oriented_Programmings/VarArgsDemo04.java
@@ -0,0 +1,20 @@
+package Object_Oriented_Programmings;
+class DifferentValues
+{
+	public void acceptHetro(Object ...obj)
+	{
+		for(Object o1 : obj)
+		{
+			System.out.println(o1);
+		}
+	}
+}
+public class VarArgsDemo04
+{
+	public static void main(String[] args) 
+	{
+		DifferentValues d1 = new DifferentValues();
+		d1.acceptHetro(10,20,309.0,"Hello",'D',new Character('A'), new String("Welcome"),10.1,"hyd-01");
+	}
+
+}
