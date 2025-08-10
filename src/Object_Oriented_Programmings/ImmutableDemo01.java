ee22916a7b71a9817642e9a18ae9022ab1d87dea
HP <saiv57177@gmail.com>
2025-08-10 22:51:29 +0530

    String class is Immutable(un-changed)

--- src/Object_Oriented_Programmings/ImmutableDemo01.java
+++ src/Object_Oriented_Programmings/ImmutableDemo01.java
@@ -1,5 +1,16 @@
 package Object_Oriented_Programmings;
 
-public class ImmutableDemo01 {
+public class ImmutableDemo01 
+{
+	public static void main(String[] args) 
+	{
+		String str = new String("Hello");
+		accept(str);
+		System.out.println(str);	
+	}
+	public static void accept(String obj)
+	{
+		obj="welcome";
+	}
 
 }
