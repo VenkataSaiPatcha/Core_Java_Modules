13db650a31fcbffc6d9c8525eefc2173970c062c
HP <saiv57177@gmail.com>
2025-08-10 19:09:07 +0530

    Describing System.out.println() statement

---
+++ src/Object_Oriented_Programmings/Description.java
@@ -0,0 +1,21 @@
+package Object_Oriented_Programmings;
+
+/*import java.io.PrintStream;
+
+public class System 
+{
+	public final static PrintStream out = null;
+
+}*/
+class Delta
+{
+	final static String str = "Hello World";
+}
+public class Description
+{
+	java.io.PrintStream out;
+	public static void main(String []args)
+	{
+		System.out.println(Delta.str.length());
+	}
+}
