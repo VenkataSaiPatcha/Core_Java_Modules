83d4a14b615c2c0caa2c6f2f2e48a683c3280c62
HP <saiv57177@gmail.com>
2025-08-10 22:26:18 +0530

    valueOf() is a predefined method available in String class used to convert any data type to String

---
+++ src/Object_Oriented_Programmings/ValueOf.java
@@ -0,0 +1,13 @@
+package Object_Oriented_Programmings;
+
+public class ValueOf 
+{
+	public static void main(String[] args) 
+	{
+		int x=100;
+		String str = String.valueOf(x);
+		System.out.println(str);
+		
+	}
+
+}
