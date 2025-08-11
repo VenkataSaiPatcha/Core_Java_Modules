2c8f4c45f9870e21a98be2b89cf472994078824d
HP <saiv57177@gmail.com>
2025-08-11 13:30:55 +0530

    .

---
+++ src/Object_Oriented_Programmings/AutoBoxing02.java
@@ -0,0 +1,13 @@
+//UnBoxing (wrapper class to primitive )
+package Object_Oriented_Programmings;
+
+public class AutoBoxing02 
+{
+	public static void main(String[] args) 
+	{
+		Character ch = 'A';
+			char c=ch.charValue();
+		System.out.println(c);
+	}
+
+}
