548dd70d7bf93f10a0e37ea9e2fae4c4d85d4e4c
HP <saiv57177@gmail.com>
2025-08-11 13:32:19 +0530

    .

---
+++ src/Object_Oriented_Programmings/AutoBoxing03.java
@@ -0,0 +1,12 @@
+package Object_Oriented_Programmings;
+
+public class AutoBoxing03 
+{
+	public static void main(String[] args) 
+	{
+		Boolean b = true;
+		boolean b1 =b.booleanValue();
+		System.out.println(b1);
+	}
+
+}
