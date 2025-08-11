1bd24b77af8fb0c27201cd6afac96a707e03d706
HP <saiv57177@gmail.com>
2025-08-11 15:40:49 +0530

    Here Object will be executed, because object class is closely related which can store any data type values

---
+++ src/Object_Oriented_Programmings/AmbiquityIssue08.java
@@ -0,0 +1,22 @@
+package Object_Oriented_Programmings;
+class Ambiquity08
+{
+	public void accept(Object obj)
+	{
+		System.out.println("object");
+	}
+	public void accept(String str)
+	{
+		System.out.println("String");
+	}
+}
+public class AmbiquityIssue08 
+{
+	public static void main(String[] args) 
+	{
+		Ambiquity08 a1 = new Ambiquity08();
+		a1.accept(10);
+		
+	}
+
+}
