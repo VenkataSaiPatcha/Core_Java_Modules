a77d0fdb476d62d59ba0e23638cb98f17ba5cf2a
HP <saiv57177@gmail.com>
2025-08-11 14:45:02 +0530

    .

---
+++ src/Object_Oriented_Programmings/AutoBoxing04.java
@@ -0,0 +1,16 @@
+package Object_Oriented_Programmings;
+
+public class AutoBoxing04
+{
+	public static void main(String[] args) 
+	{
+		long l = 12; // widening / upcasting /Implicit
+		byte b = (byte)12L; // narrowing / downcasting / explicit
+		
+		Double d = 12D;
+		Long l1 = 13L;
+		
+		
+	}
+
+}
