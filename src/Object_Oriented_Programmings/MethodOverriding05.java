0882ad9361b46dda650a129bc0f2b3cc8caa9703
HP <saiv57177@gmail.com>
2025-08-11 18:51:09 +0530

    Variable Hiding and method overriding with inheritance

---
+++ src/Object_Oriented_Programmings/MethodOverriding05.java
@@ -0,0 +1,29 @@
+package Object_Oriented_Programmings;
+
+class RBI
+{
+	protected String ifscCode = "RBIHYD09675";
+	public String loan() {
+		return "RBI approved loan";
+	}
+}
+class SBI extends RBI
+{
+	protected String ifscCode = "SBIAMPT78645"; // variable Hiding
+	@Override
+	public String loan()
+	{
+		return "Providing loan with some interest";
+	}
+}
+public class MethodOverriding05 
+{
+	public static void main(String[] args) 
+	{
+		RBI r = new SBI();
+		System.out.println(r.ifscCode);
+		System.out.println(r.loan());
+		
+	}
+
+}
