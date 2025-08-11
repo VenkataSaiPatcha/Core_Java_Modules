c8cfb76c678863278e147ff9203a470dd8c8d00f
HP <saiv57177@gmail.com>
2025-08-12 00:07:20 +0530

    .

---
+++ src/LogicalPrograms/EvenAndOdd.java
@@ -0,0 +1,28 @@
+package LogicalPrograms;
+
+public class EvenAndOdd 
+{
+	public static void main(String[] args) 
+	{
+		int n=10;
+//		if(n/2*2==n)
+//		{
+//			System.out.println("Even");
+//		}
+//		else
+//		{
+//			System.out.println("Not Even");
+//		}
+		
+		if((n&1)==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+		
+	}
+
+}
