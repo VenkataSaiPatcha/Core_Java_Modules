74931d88d61a80e82f13757c64e4bd2a90147a20
HP <saiv57177@gmail.com>
2025-08-11 12:35:03 +0530

    In Integer class having toString(),hashcode() and equals() methods to perform some operations

---
+++ src/Object_Oriented_Programmings/IntToString.java
@@ -0,0 +1,12 @@
+package Object_Oriented_Programmings;
+
+public class IntToString 
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		String str = Integer.toString(i);
+		System.out.println(str+2);
+	}
+
+}
