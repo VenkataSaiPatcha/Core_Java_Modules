7d30b15216db93db6f6225d08ec072a1e8b514a8
HP <saiv57177@gmail.com>
2025-08-15 13:58:55 +0530

    In the above program when we pass Dog object then we will get Runtime Exception java.lang.ClassCastException becuase Dog can't be converted into Lion.

---
+++ src/Object_Oriented_Programmings/PolymorphicDemo03.java
@@ -0,0 +1,53 @@
+package Object_Oriented_Programmings;
+
+class Animal03
+{
+	public void roar()
+	{
+		System.out.println("Generic Animal is roaring");
+	}
+}
+class Lion03 extends Animal03
+{
+	@Override
+	public void roar()
+	{
+		System.out.println("Lion animal is roaring");
+	}
+	
+	public void roam()
+	{
+		System.out.println("Lion03.roam()");
+		
+	}
+}
+class Dog03 extends Animal03
+{
+	
+	@Override
+	public void roar()
+	{
+		System.out.println("Dog animal is roaring");
+	}
+}
+
+public class PolymorphicDemo03 
+{
+	public static void main(String[] args) 
+	{
+		Animal03 a1 = null;
+		a1= new Lion03();
+		animalRoar(a1);
+		 
+		a1 = new Dog03();
+		animalRoar(a1);
+		
+	}
+	public static void animalRoar(Animal03 animal)
+	{
+		Lion03 l1 = (Lion03)animal;
+		l1.roar();
+		l1.roam();
+	}
+
+}
