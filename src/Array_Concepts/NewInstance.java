60469bc4a6813a3f4ee21e924fae977c8ca7d384
HP <saiv57177@gmail.com>
2025-08-15 19:03:37 +0530

    Array creation using newInstance() method

---
+++ src/Array_Concepts/NewInstance.java
@@ -0,0 +1,18 @@
+package Array_Concepts;
+
+import java.lang.reflect.Array;
+// using newInstance() method we create an array
+public class NewInstance 
+{
+	public static void main(String[] args) 
+	{
+		int arr[]=(int[]) Array.newInstance(int.class,5);  // 1d array
+		
+		Integer i1[][]=(Integer[][])Array.newInstance(Integer.class, 10,10); // 2d array
+		
+		Float f[][][][][]=(Float[][][][][])Array.newInstance(Float.class,5,6,7,8,9);  // multi dimensional array
+		
+		
+	}
+
+}
