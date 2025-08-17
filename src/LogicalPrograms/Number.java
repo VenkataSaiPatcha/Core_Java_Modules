756e83b1c62028461929d1f17a1155c5b61ad605
HP <saiv57177@gmail.com>
2025-08-17 15:04:06 +0530

    .

---
+++ src/LogicalPrograms/Number.java
@@ -0,0 +1,14 @@
+package LogicalPrograms;
+
+import java.util.Scanner;
+
+public class Number
+{
+	public static void main(String[] args) 
+	{
+		 Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter number : ");
+	       int n=sc.nextInt();
+	}
+
+}
