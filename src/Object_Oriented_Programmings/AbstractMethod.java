228092cccbd6105ab42f3603f3e28e2f993d6294
HP <saiv57177@gmail.com>
2025-08-17 11:31:57 +0530

    Abstract class and abstract method

---
+++ src/Object_Oriented_Programmings/AbstractMethod.java
@@ -0,0 +1,39 @@
+package Object_Oriented_Programmings;
+
+abstract class Vehicle03
+{
+	public abstract void run();
+}
+class Car03 extends Vehicle03
+{
+	@Override
+	public void run() 
+	{
+		
+	}
+}
+class Bike03 extends Vehicle03
+{
+	@Override
+	public void run()
+	{
+		
+	}
+}
+abstract class Bus03 extends Vehicle03
+{
+	/*@Override
+	public void run()
+	{
+		
+	} */
+}
+public class AbstractMethod 
+{
+	public static void main(String[] args) 
+	{
+		
+		
+	}
+
+}
