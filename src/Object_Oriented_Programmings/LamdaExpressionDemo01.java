5860a9d0bb603d1f5433e4f642a08419d2f850d9
HP <saiv57177@gmail.com>
2025-08-18 17:55:08 +0530

    Lambda Expression from JDK 1.8 to consice the code or remove boiler plate code

---
+++ src/Object_Oriented_Programmings/LamdaExpressionDemo01.java
@@ -0,0 +1,25 @@
+package Object_Oriented_Programmings;
+
+interface Computer
+{
+	void laptop();
+}
+public class LamdaExpressionDemo01
+{
+	public static void main(String[] args) 
+	{
+		/*Computer c1 = new Computer()  normal ananimous inner class to call the abstract method inside the interface 
+		{	
+			@Override
+			public void laptop()
+			{
+				System.out.println("laptop is less weight compare to pc");
+			}
+		};
+		c1.laptop(); */
+		
+		Computer c2 =()->System.out.println("MacBook air is high secure");
+		c2.laptop();
+	}
+
+}
