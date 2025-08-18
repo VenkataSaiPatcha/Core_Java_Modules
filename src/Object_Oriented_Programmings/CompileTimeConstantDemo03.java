ea71177863959f65c702377c9de8821e1b314cfe
HP <saiv57177@gmail.com>
2025-08-18 16:26:02 +0530

    Compile time constant

---
+++ src/Object_Oriented_Programmings/CompileTimeConstantDemo03.java
@@ -0,0 +1,14 @@
+package Object_Oriented_Programmings;
+
+interface HelloWorld
+{
+	public static final int x=300;
+}
+public class CompileTimeConstantDemo03 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(HelloWorld.x);	
+	}
+
+}
