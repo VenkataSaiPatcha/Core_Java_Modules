f6302ae7b0247583c2c772ed2f2d5472d5fa4153
HP <saiv57177@gmail.com>
2025-08-19 17:06:34 +0530

    .

--- src/Array_Concepts/ThreeDJaggedArray.java
+++ src/Array_Concepts/ThreeDJaggedArray.java
@@ -1,19 +1,48 @@
 package Array_Concepts;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ThreeDJaggedArray 
 {
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size");
-		int size =sc.nextInt();
-		int arr[][][] = new int[size][][];
-		System.out.println("Enter the "+arr.length+" values");
-		for(int i=0;i<arr.length;i++)
+		int arr[][][] = new int[3][][]; // 3d jagged array
+		arr[0] = new int[3][];
+		arr[1] = new int[4][];
+		arr[2] = new int[5][];
+		
+		arr[0][0] = new int[5];
+		arr[0][1] = new int[6];
+		arr[0][2] = new int[3];
+		
+		arr[1][0] = new int[2];
+		arr[1][1] = new int[3];
+		arr[1][2] = new int[5];
+		arr[1][3] = new int[4];
+		
+		arr[2][0] = new int[5];
+		arr[2][1] = new int[7];
+		arr[2][2] = new int[3];
+		arr[2][3] = new int[2];
+		arr[2][4] = new int[8];
+		
+		
+		arr[0][0].length+
+		
+		
+		for(int x[][]:arr)
 		{
-			
+			for(int y[]:x)
+			{
+				for(int z:y)
+				{
+					System.out.print(z+" ");
+				}
+				System.out.println();
+				
+			}
+			System.out.println();
 		}
 	}
 
