0b986705822f7b590abb3f582214906cc693c140
HP <saiv57177@gmail.com>
2025-08-19 14:35:06 +0530

    we can also write main method inside an interface because interface is internally contains abstract class

---
+++ src/Object_Oriented_Programmings/Java8StaticMethodDemo02.java
@@ -0,0 +1,10 @@
+package Object_Oriented_Programmings;
+
+public interface Java8StaticMethodDemo02
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World !!!");
+	}
+
+}
