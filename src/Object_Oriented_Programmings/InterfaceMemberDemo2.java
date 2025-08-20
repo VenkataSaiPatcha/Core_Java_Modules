ae342e61911068f79c3a247bfbe2bb294d649b3d
HP <saiv57177@gmail.com>
2025-08-20 18:06:13 +0530

    It is clear that we can't override Object class public method as a default method inside interface.

---
+++ src/Object_Oriented_Programmings/InterfaceMemberDemo2.java
@@ -0,0 +1,25 @@
+package Object_Oriented_Programmings;
+
+interface Flash
+{
+	/*default String toString() //error
+	{
+		
+	} */
+	/* default int hashCode() // error
+	{
+		
+	} */
+}
+class Light implements Flash
+{
+	
+}
+public class InterfaceMemberDemo2 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
