ee4ce59fabda8e4d0347984e4e2aa047862c68e8
HP <saiv57177@gmail.com>
2025-08-26 15:26:11 +0530

    Types of Exceptions

---
+++ src/ExceptionalHandling/Exceptions.java
@@ -0,0 +1,30 @@
+package ExceptionalHandling;
+
+import java.util.Scanner;
+
+public class Exceptions 
+{
+	public static void main(String[] args) 
+	{
+		int x = 10;
+		int y = 0;
+		//System.out.println(x/y);
+		
+		int arr[] = {10,20,30};
+		//System.out.println(arr[3]);
+		
+		String str = "Hyderabad";
+		//System.out.println(str.substring(-2, 9));
+		
+		//int arr1[] = new int[-5];
+		
+		String s1 = null;
+		//System.out.println(s1.length());
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int value =sc.nextInt();
+		
+	}
+
+}
