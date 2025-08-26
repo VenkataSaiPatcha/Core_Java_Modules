6070ed161cc74b595e13102ffc183fca222a467a
HP <saiv57177@gmail.com>
2025-08-26 16:08:02 +0530

    In the above program, If we enter the value of y as 0 then our program will be terminated abnormally, JVM has default exception handler, which will terminate the program in the middle (abnormal termination) and provide the execption message with line number.

---
+++ src/ExceptionalHandling/AbnormalTermination.java
@@ -0,0 +1,18 @@
+package ExceptionalHandling;
+
+import java.util.Scanner;
+
+public class AbnormalTermination
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x value ");
+		int x =sc.nextInt();
+		System.out.println("Enter y value ");
+		int y = sc.nextInt();
+		int z =x/y;
+		System.out.println("Result is :"+z);
+	}
+
+}
