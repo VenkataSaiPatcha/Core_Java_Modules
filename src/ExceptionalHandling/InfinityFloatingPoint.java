41161e2b8f5584cbf297af0c9688f99cd406c5c5
HP <saiv57177@gmail.com>
2025-08-26 17:42:15 +0530

    In Java contains final+static variables to perform handling on Float and Double values only

---
+++ src/ExceptionalHandling/InfinityFloatingPoint.java
@@ -0,0 +1,16 @@
+package ExceptionalHandling;
+
+public class InfinityFloatingPoint 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method started");
+		System.out.println(10/0.0);
+		System.out.println(-18/0.0);
+		System.out.println(0/0.0);
+		System.out.println(0.0);
+		System.out.println(10/0);
+		System.out.println("Main method ended");
+	}
+
+}
