6fb81493805a767fa960fa64d306b1a1b635c5b3
HP <saiv57177@gmail.com>
2025-08-26 15:31:37 +0530

    .

---
+++ src/Array_Concepts/ArrayClassConcept.java
@@ -0,0 +1,40 @@
+package Array_Concepts;
+
+import java.lang.reflect.Array;
+
+public class ArrayClassConcept 
+{
+	public static void main(String[] args) 
+	{
+//		Object obj[]= {10,20.0,"sai",new Object().toString(),'A',120L};
+//			System.out.println(Array.get(obj, 3));
+		
+//		boolean arr[] = {true,false,true,true,false};
+//		System.out.println(Array.getBoolean(arr, 0));
+		
+//		byte b[] = {123,124,125,126,127,-128};
+//		System.out.println(Array.getByte(b, 5));
+		
+//		char ch[]= {'A','B','C','D'};
+//		System.out.println(Array.getChar(ch, 3));
+		
+//		double d[]= {11.1,11.2,11.3,11.4,11.5,11.6,11.7};
+//		System.out.println(Array.getDouble(d, 0));
+		
+//		float f[]  = {12.0f,12.1f,12.2f,12.3f};
+//		System.out.println(Array.getFloat(f, 0));
+		
+//		int arr[] = {12,13,14,15,16,17,18,19};
+//		System.out.println(Array.getInt(arr,5));
+		
+//		int len[]= {10,20,30,40,50};
+//		System.out.println(Array.getLength(len));
+		
+//		long l[]= {12L,13L,14L,15L,16L};
+//		System.out.println(Array.getLong(l, 0));
+		
+//		short s[]= {12,13,14,15,16,17,18};
+//		System.out.println(Array.getShort(s, 0));
+	}
+
+}
