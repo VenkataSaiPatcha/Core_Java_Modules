1ee00923615467a4df80c08ece5e41a9d2153429
HP <saiv57177@gmail.com>
2025-08-28 19:26:42 +0530

    Initialization of a variable in try and catch :

---
+++ src/ExceptionalHandling/VariableInitialization.java
@@ -0,0 +1,23 @@
+package ExceptionalHandling;
+
+public class VariableInitialization 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main method started");
+		int x; // declaration
+		try
+		{
+			 x=100; // local variable initialization
+			System.out.println(x);
+		}
+		catch(Exception e)
+		{
+		 x=200;
+			System.out.println(x);
+			e.printStackTrace();
+		}
+		System.out.println("main method ended");
+	}
+
+}
