df45ec5c14a54d601759e8548d2384c86e8db14b
HP <saiv57177@gmail.com>
2025-08-29 14:53:30 +0530

    Enum contains internally a .class file

--- src/EnumConcepts/Color.java
+++ src/EnumConcepts/Color.java
@@ -1,7 +1,14 @@
 package EnumConcepts;
 
-public enum Color 
+enum ColorClass 
 {
 	RED,BLUE,BLACK; // public + static + final (enum constants)
-
+	
+}
+public class Color
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(ColorClass.BLACK);
+	}
 }
