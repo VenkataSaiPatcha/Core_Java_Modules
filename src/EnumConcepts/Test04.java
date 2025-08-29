29037c86ddb46b330a9cd7607bb4680750613b4c
HP <saiv57177@gmail.com>
2025-08-29 15:11:14 +0530

    An enum defined inside a class can be private, static, default, public and protected, It can't be final and abstract.

---
+++ src/EnumConcepts/Test04.java
@@ -0,0 +1,15 @@
+package EnumConcepts;
+
+public class Test04 
+{
+	private enum Seasons
+	{
+		Summer, Rainy, Winter, Spring;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(Seasons.Summer);
+		
+	}
+
+}
