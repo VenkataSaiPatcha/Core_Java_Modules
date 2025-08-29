55f910fd0a65981da1156695414c3a1c769e1531
HP <saiv57177@gmail.com>
2025-08-29 15:23:48 +0530

    We can take main () inside an enum, After enum constant, if we decalre any member then ; is compulsory

---
+++ src/EnumConcepts/Test08.java
@@ -0,0 +1,12 @@
+package EnumConcepts;
+
+public enum Test08 
+{
+	TestT1, TestT2, TestT3;
+	public static void main(String []args)
+	{
+		System.out.println("Main method");
+		System.out.println(Test08.TestT3);
+	}
+
+}
