7c7a2cac1291e34b38a626424b6699519a76cd5f
HP <saiv57177@gmail.com>
2025-08-29 14:48:54 +0530

    we can also write enum inside a method

---
+++ src/EnumConcepts/Test01.java
@@ -0,0 +1,15 @@
+package EnumConcepts;
+
+public class Test01 
+{
+	public static void main(String[] args) 
+	{
+		enum Month
+		{
+			January, June, September, December;
+		}
+		System.out.println(Month.December);
+		
+	}
+
+}
