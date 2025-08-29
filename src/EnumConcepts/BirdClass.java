b59ed07a6393747108056e5df8e83e96ab56d998
HP <saiv57177@gmail.com>
2025-08-29 14:45:44 +0530

    we can access enum constants by using enum class name

---
+++ src/EnumConcepts/BirdClass.java
@@ -0,0 +1,14 @@
+package EnumConcepts;
+
+enum Bird
+{
+	Peacock,Parrot,Sparrow;
+}
+public class BirdClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Bird.Parrot);
+	}
+
+}
