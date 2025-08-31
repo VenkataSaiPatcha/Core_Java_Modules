efc1cc6a048355c40e6883eef813e819b440c439
HP <saiv57177@gmail.com>
2025-08-31 20:16:25 +0530

    .

---
+++ src/InterviewConcept/DemoClass.java
@@ -0,0 +1,34 @@
+package InterviewConcept;
+
+public class DemoClass 
+{
+	private int a;
+	private int b;
+	
+	public DemoClass(int a, int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	public DemoClass()
+	{
+		
+	}
+	public void setA(int a)
+	{
+		this.a = a;
+	}
+	public int getA()
+	{
+		return a;
+	}
+	
+	public void setB(int b)
+	{
+		this.b = b;
+	}
+	public int getB()
+	{
+		return b;
+	}
+}
