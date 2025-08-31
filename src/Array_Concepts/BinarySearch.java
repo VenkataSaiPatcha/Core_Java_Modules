1a034a9793a5d1f063dc0171a08464ab7db4dce0
HP <saiv57177@gmail.com>
2025-08-31 20:14:55 +0530

    .

---
+++ src/Array_Concepts/BinarySearch.java
@@ -0,0 +1,15 @@
+package Array_Concepts;
+
+import java.util.Arrays;
+
+public class BinarySearch
+{
+	public static void main(String[] args) 
+	{
+		 byte[]b = {10,20,30,40,50};
+		 System.out.println(Arrays.binarySearch(b, (byte)30)); 
+		 System.out.println(Arrays.binarySearch(b, (byte)25)); 
+		
+	}
+
+}
