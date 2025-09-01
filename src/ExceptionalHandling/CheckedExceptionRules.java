2eeef39b861f5e682767e4e2318f0abd4e64235d
HP <saiv57177@gmail.com>
2025-09-01 15:23:47 +0530

    While dealing with checked exception we can throw the exception from try block to catch block. And it is only applicable to checked exception

---
+++ src/ExceptionalHandling/CheckedExceptionRules.java
@@ -0,0 +1,33 @@
+package ExceptionalHandling;
+
+import java.util.InputMismatchException;
+
+//If the try block does not throw any checked exception then in the corresponding
+//catch block we can't handle checked exception.It will generate compilation error 
+//i.e "exception never thrown from the corresponding try statement"
+public class CheckedExceptionRules
+{
+	public static void main(String[] args) 
+	{
+		/*try
+		{
+			
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		 */
+		
+		//  The above rule is not applicable for Unchecked Exception
+		try
+		{
+			
+		}
+		catch(ArithmeticException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
