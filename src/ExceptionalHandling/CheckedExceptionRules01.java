c10fc644dba37397e3809065b00e4b756a77266a
HP <saiv57177@gmail.com>
2025-09-01 15:27:47 +0530

    .

--- src/ExceptionalHandling/CheckedExceptionRules01.java
+++ src/ExceptionalHandling/CheckedExceptionRules01.java
@@ -5,7 +5,7 @@ import java.util.InputMismatchException;
 //If the try block does not throw any checked exception then in the corresponding
 //catch block we can't handle checked exception.It will generate compilation error 
 //i.e "exception never thrown from the corresponding try statement"
-public class CheckedExceptionRules
+public class CheckedExceptionRules01
 {
 	public static void main(String[] args) 
 	{
