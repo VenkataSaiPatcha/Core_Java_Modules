e8048bb8f17bf6751875ee352e1e8608c9a6cd26
HP <saiv57177@gmail.com>
2025-09-02 17:43:26 +0530

    Using isAlive() method we define thread is started or not

---
+++ src/MultiThread_Concepts/CheckingThreadIsAlive.java
@@ -0,0 +1,23 @@
+package MultiThread_Concepts;
+
+class MyThreadClass extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("Child thred is running in separate stack memory");
+	}
+}
+public class CheckingThreadIsAlive 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method started");
+		MyThreadClass thread = new MyThreadClass();
+		System.out.println("Is thread is alive :"+thread.isAlive());
+		thread.start();
+		System.out.println("Is thread is alive :"+thread.isAlive());
+		System.out.println("Main method ended");
+	}
+
+}
