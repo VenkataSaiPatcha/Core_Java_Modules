20f3691033e69b4689a3dc33d4c01d2b78bf327d
HP <saiv57177@gmail.com>
2025-09-02 17:00:00 +0530

    Program to show main is a thread with method chaning :

---
+++ src/MultiThread_Concepts/MainMethodThread01.java
@@ -0,0 +1,11 @@
+package MultiThread_Concepts;
+
+public class MainMethodThread01 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("current thread name is : "+Thread.currentThread().getName());
+		
+	}
+
+}
