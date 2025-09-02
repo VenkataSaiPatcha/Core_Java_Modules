4265d1c381e1769ba009e40f9fc3252ccc9b6977
HP <saiv57177@gmail.com>
2025-09-02 18:32:17 +0530

    .

--- src/Array_Concepts/BinarySearch.java
+++ src/Array_Concepts/BinarySearch.java
@@ -6,9 +6,14 @@ public class BinarySearch
 {
 	public static void main(String[] args) 
 	{
-		 byte[]b = {10,20,30,40,50};
+	/*	 byte[]b = {10,20,30,40,50};
 		 System.out.println(Arrays.binarySearch(b, (byte)30)); 
 		 System.out.println(Arrays.binarySearch(b, (byte)25)); 
+		*/
+		
+//		byte b[] = {10,20,30,40,50,60,70,80};
+//		System.out.println(Arrays.binarySearch(b, 0,4,(byte)50));
+		
 		
 	}
 
