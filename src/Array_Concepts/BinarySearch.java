61ee07a9a00dd619244bdc3e764461ba2731b9ac
HP <saiv57177@gmail.com>
2025-09-02 18:48:16 +0530

    .

--- src/Array_Concepts/BinarySearch.java
+++ src/Array_Concepts/BinarySearch.java
@@ -21,8 +21,14 @@ public class BinarySearch
 //		char ch[] = {'A','B','C','D','E','F','G','H'};
 //		System.out.println(Arrays.binarySearch(ch, 2, 4,'D'));
 		
-		double d[] = {10.0,10.2,10.3,10.4,10.5,10.6,10.7};
-		System.out.println(Arrays.binarySearch(d, 10.5));
+//		double d[] = {10.0,10.2,10.3,10.4,10.5,10.6,10.7};
+//		System.out.println(Arrays.binarySearch(d, 10.5));
+		
+//		double d[] = {10.0,10.2,10.3,10.4,10.5,10.6,10.7};
+//		System.out.println(Arrays.binarySearch(d,2,6,10.6));
+		
+		float f[] = {0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f};
+		System.out.println(Arrays.binarySearch(f,(float)0.2));
 	}
 
 }
