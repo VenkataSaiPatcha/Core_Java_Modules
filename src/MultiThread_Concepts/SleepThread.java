1798d86c59cae3806a071d6f4146f59e6689a793
HP <saiv57177@gmail.com>
2025-09-03 16:48:01 +0530

    sleep() method used to put a thread temporarly in waiting state

---
+++ src/MultiThread_Concepts/SleepThread.java
@@ -0,0 +1,30 @@
+package MultiThread_Concepts;
+
+class SampleThreadClass extends Thread
+{
+	@Override
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println("Thread is interrupted :"+e);
+			}
+		}
+	}
+}
+public class SleepThread 
+{
+	public static void main(String[] args) 
+	{
+		SampleThreadClass s1 = new SampleThreadClass();
+		s1.start();
+	}
+
+}
