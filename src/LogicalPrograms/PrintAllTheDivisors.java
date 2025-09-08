8c339117e72da664cca2c87fd5af30be0daa7a59
HP <saiv57177@gmail.com>
2025-09-08 16:21:57 +0530

    .

---
+++ src/LogicalPrograms/PrintAllTheDivisors.java
@@ -0,0 +1,19 @@
+package LogicalPrograms;
+
+import java.util.Scanner;
+
+public class PrintAllTheDivisors
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value");
+		int num = sc.nextInt();
+		PrintAllTheDivisors p1 = new PrintAllTheDivisors();
+		p1.devisiors(num);
+	}
+	public int devisiors(int num)
+	{
+		return 0;
+	}
+}
