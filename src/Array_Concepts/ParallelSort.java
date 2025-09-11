d2ed26fb14c7e3ba51cf5f16f92d85a4eb0d9d94
HP <saiv57177@gmail.com>
2025-09-11 19:02:02 +0530

    .

--- src/Array_Concepts/ParallelSort.java
+++ src/Array_Concepts/ParallelSort.java
@@ -1,6 +1,8 @@
 package Array_Concepts;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class ParallelSort 
 {
@@ -10,6 +12,20 @@ public class ParallelSort
 //		Arrays.parallelSort(arr);
 //		System.out.println(Arrays.toString(arr));
 		
+//		String [] str = {"sai","kiran","ramesh","vishnu","mahesh"};
+//		String [] str1 = {"raju","ramesh","hari","akash","amit"};
+//		Arrays.parallelSort(str,Comparator.naturalOrder());
+//		Arrays.parallelSort(str, Comparator.reverseOrder());
+//		System.out.println(Arrays.toString(str));
+		
+//		int arr[] = {1,2,3,4,5,6,7};
+//		System.out.println("Arrays size is :"+arr.length);
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the size");
+//		int size = sc.nextInt();
+//		int arr1[] = new int[size];
+//		System.out.println(arr1.length);
+//		System.out.println(Arrays.toString(arr1));
 	}
 
 }
