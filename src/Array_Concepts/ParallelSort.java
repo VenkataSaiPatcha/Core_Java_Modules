3567822a1bebe9f85f319a2f1e37f04a6e42ad0a
HP <saiv57177@gmail.com>
2025-09-11 18:44:08 +0530

    .

---
+++ src/Array_Concepts/ParallelSort.java
@@ -0,0 +1,15 @@
+package Array_Concepts;
+
+import java.util.Arrays;
+
+public class ParallelSort 
+{
+	public static void main(String[] args) 
+	{
+//		Integer [] arr = {43,23,1,5,6,90};
+//		Arrays.parallelSort(arr);
+//		System.out.println(Arrays.toString(arr));
+		
+	}
+
+}
