8aeda8b06f9406b9483999e85a0fcc6aa7621ffc
HP <saiv57177@gmail.com>
2025-09-14 16:25:42 +0530

    We can use the Object class method's inside the synchronized area only other wise we will get java.lang.IllegalMonitorStateException

---
+++ src/MultiThread_Concepts/InterThreadCommunicationDemo01.java
@@ -0,0 +1,12 @@
+package MultiThread_Concepts;
+
+public class InterThreadCommunicationDemo01 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		Object obj = new Object();
+		obj.wait();
+		
+	}
+
+}
