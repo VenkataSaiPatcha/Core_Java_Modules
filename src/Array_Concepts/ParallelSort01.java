f85a8569b186da09c25a64d7d9685bbb3291d4d1
HP <saiv57177@gmail.com>
2025-09-17 19:03:59 +0530

    .

--- src/Array_Concepts/ParallelSort01.java
+++ src/Array_Concepts/ParallelSort01.java
@@ -7,9 +7,16 @@ public class ParallelSort01
 {
 	public static void main(String[] args) 
 	{
-		String [] str = {"Ravi","Ankith","Kiran","Manoj","Sunitha","Amith"};
-		Arrays.parallelSort(str,2,6,Comparator.naturalOrder());
-		System.out.println(Arrays.toString(str));
+//		String [] str = {"Ravi","Ankith","Kiran","Manoj","Sunitha","Amith"};
+//		Arrays.parallelSort(str,2,6,Comparator.naturalOrder());
+//		System.out.println(Arrays.toString(str));
+		
+//		double [] d = new double[5];
+//		
+//		Arrays.setAll(d, (i)-> i*i);
+//		System.out.println(Arrays.toString(d));
+		
+		
 	}
 
 }
