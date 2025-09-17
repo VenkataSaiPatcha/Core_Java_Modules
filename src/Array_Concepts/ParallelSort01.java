716afef481543ee2572cfebcad9272d9d7ad2c71
HP <saiv57177@gmail.com>
2025-09-17 18:18:08 +0530

    .

---
+++ src/Array_Concepts/ParallelSort01.java
@@ -0,0 +1,15 @@
+package Array_Concepts;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ParallelSort01 
+{
+	public static void main(String[] args) 
+	{
+		String [] str = {"Ravi","Ankith","Kiran","Manoj","Sunitha","Amith"};
+		Arrays.parallelSort(str,2,6,Comparator.naturalOrder());
+		System.out.println(Arrays.toString(str));
+	}
+
+}
