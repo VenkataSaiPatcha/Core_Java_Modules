cf2fe4e2a53fc9f0311fc1f03bf2b91696566372
HP <saiv57177@gmail.com>
2025-09-18 17:57:28 +0530

    .

---
+++ src/MultiThread_Concepts/ThreadGroupDemo03.java
@@ -0,0 +1,12 @@
+package MultiThread_Concepts;
+
+public class ThreadGroupDemo03 
+{
+	public static void main(String[] args) 
+	{
+		Thread t1 = Thread.currentThread();
+		System.out.println(t1.toString());
+		
+	}
+
+}
